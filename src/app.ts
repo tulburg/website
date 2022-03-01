1a3eef813752c1a7b1ad9435e2b2f7198550a6fb
Tolu Oluwagbemi <tolu@anotemusic.com>
2022-03-01 23:30:28 +0100

    Update app.ts + add svg.ts for infinite dreams

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,57 @@
-import { PageComponent, Container } from "@js-native/core/components";
+import { PageComponent, SVG } from "@js-native/core/components";
+import svg from "./assets/svg";
 
 export default class App extends PageComponent {
 
+  infinite: SVG;
+  infiniteOverlay: SVG;
+  dreams: SVG;
+  dreamsOverlay: SVG;
+
   constructor() {
     super();
-
-    const container = new Container();
-    container.size([500, 500]).backgroundColor('green').borderRadius(20);
-    this.addChild(container, new Container().size([100, 200]).backgroundColor('pink'));
+    this.infiniteOverlay = new SVG().width(446).height(280).viewBox('0 0 446 280').fill('none')
+      .position('absolute').left('50%').transform('translateX(-50%) scale(1.8)')
+      .top(200);
+    this.infinite = new SVG().width(446).height(280).viewBox('0 0 446 280').fill('none')
+      .position('absolute').left('50%').transform('translateX(-50%) scale(1.8)')
+      .top(200);
+    this.dreamsOverlay = new SVG().width(501).height(222).viewBox('0 0 501 222').fill('none')
+      .position('absolute').left('60%').transform('translateX(-50%) scale(1.8)')
+      .top(380);
+    this.dreams = new SVG().width(501).height(222).viewBox('0 0 501 222').fill('none')
+      .position('absolute').left('60%').transform('translateX(-50%) scale(1.8)')
+      .top(380);
+    this.addChild(this.infinite, this.infiniteOverlay, this.dreams, this.dreamsOverlay);
   }
 
   onCreate() { 
+    this.infinite.node().innerHTML = svg.infinite;
+    this.infiniteOverlay.node().innerHTML = svg.infinite;
+    this.dreams.node().innerHTML = svg.dreams;
+    this.dreamsOverlay.node().innerHTML = svg.dreams;
+
+    let delay = 0;
+    [
+      this.infinite.node(), this.infiniteOverlay.node(),
+      this.dreams.node(), this.dreamsOverlay.node()
+    ].forEach((svg, jindex) => {
+      if(jindex === 1) delay = 0.25;
+        if(jindex === 2) delay = 4;
+        if(jindex === 3) delay = 4.25;
+      svg.querySelectorAll('path').forEach((path: any, index) => {
+        const pathLength = path.getTotalLength(), duration = pathLength / 500;
+        path.setAttribute('id', 'exp-' + index);
+        if(jindex === 0 || jindex === 2) path.style = `stroke: url(#gradient); stroke-dasharray: ${pathLength}; stroke-dashoffset: ${pathLength}; stroke-opacity: 1`;
+        else path.style = `stroke: white; stroke-dasharray: ${pathLength}; stroke-dashoffset: ${pathLength}; stroke-opacity: 1`;
+        if(index === 0) {
+          path.innerHTML = `
+            <animate id="anim${index}" attributeName="stroke-dashoffset" begin="${delay}s" dur="${duration}" to="0" fill="freeze" delay="${delay}" keySplines="0; 0.25; 0.1; 0.5; 1">
+          `;
+        }else path.innerHTML = `
+          <animate attributeName="stroke-dashoffset" id="anim${index}" begin="anim${index-1}.end + ${delay}s" dur="${duration}" to="0" fill="freeze" delay="${delay}" keySplines="0; 0.25; 0.1; 0.5; 1">
+        `;
+      })
+    })
   }
 }
---
+++ src/assets/svg.ts
@@ -0,0 +1,32 @@
+export default { 
+  infinite:  
+    `<defs>
+      <linearGradient id="gradient" x1="0" x2="0" y1="0" y2="1">
+        <stop offset="0%" stop-color="#4BC1EC"/>
+        <stop offset="50%" stop-color="#6C50AF"/>
+        <stop offset="100%" stop-color="#D92D71"/>
+      </linearGradient>
+    </defs>
+    <path d="M49.6228 99.5C35.1228 106.5 1.72281 120.4 4.12281 112C40.1229 83 161.623 42.5 221.623 28C222.79 28 226.023 28.5 229.623 30.5" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M103.623 55C81.1228 87 46.0228 205.9 35.6228 259.5" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M4.1228 276C18.6228 266.833 54.8228 248.4 83.6228 248" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M83.1226 178C79.6226 186.333 68.6228 208.5 71.6227 210.5C80.2894 197.667 100.923 171.392 106.123 173.792C111.623 174.5 107.227 201.558 117.288 196.292C122.623 193.5 139.623 176.5 142.788 173.792" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M191.123 71C191.956 66.5 193.123 59.6999 191.123 58.4999C188.623 56.9999 117.623 218 133.623 238.5C147.123 239.5 167.623 195.5 146.123 187.5C142.903 186.302 135.223 184.3 129.623 187.5L187.123 160" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M196.123 148C190.623 156 181.588 177.9 191.988 177.5C198.867 177.235 214.623 161.5 224.488 152.5" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M203.123 131.5L205.623 125" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M232.123 138.5C226.789 148.333 216.388 169.9 217.988 171.5C219.988 173.5 247.123 132 254.488 134C260.76 135.703 253.123 148.5 262.623 156C267.54 159.882 281.123 141.5 289.488 136.5" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M299.988 120C293.988 128.5 284.004 154.678 295.488 150C308.988 144.5 320.488 130.5 327.488 125.5" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M306.488 103.5L308.988 97" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M383.788 20.2923C385.954 17.459 390.288 9.7923 390.288 4.79219C390.288 3.29229 388.109 4.29217 387.788 4.79219C361.788 45.2922 316.288 135.792 330.288 152.292C336.788 160.292 369.288 126.292 384.788 106.292M322.288 63.7923C348.121 55.6256 400.488 39.9923 403.288 42.7923" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M436.623 93C420.623 112.5 382.623 142 382.623 114.5C383.623 102 397.623 86 404.623 88C414.123 91 397.623 117.5 387.123 109" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    `,
+  dreams: 
+  `<path d="M106 58C111.833 45 125 17 125 13C97.5 49.5 59.5 151.5 50.5 192" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M39.0002 110C24.1668 107.333 -2.60006 96.7 7.00002 69.5001C20.9413 30 156.5 -30.5 175.5 31C194.5 92.5 92.0001 221 15.5001 217.5" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M181.5 107C182.5 101.833 187.9 93.5 185.5 93.5C182.5 93.5 175 112.5 179.5 112.5C183.1 112.5 197.167 102.667 203 98.5C197.5 112 187.225 137.163 197.5 135C207 133 226 106.5 234.5 99" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M237.496 100C241.163 100.833 249.796 100 254.996 90C261.496 77.5 254.001 75.5 249 79C242.761 83.3665 220.5 115 246 115C257.598 115 274.996 94.5001 281.496 90" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M310.5 72.9999C308 67.1666 300.6 61.2 289 80C274.5 103.5 289.5 111.5 303 92.5C313.8 77.3 322 66.6667 323.5 63.5C320.667 72.1667 313.7 92.5 322.5 94.5C331.3 96.5 349 74.8333 354.5 65.5" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M357.5 57.4999L348 87.4999C359 71.5 387 33.4998 394 40.4999C396.5 42.9999 392.5 58.4999 392.5 65.9999L411.5 47.9999C415.167 44.6666 421.637 45.1117 419.5 60.5C417 78.5 438 54.4999 449 43.9999" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M450.501 42.5001C457.001 35.5001 468.97 16.1309 461.002 21.0001C443.001 32 457.441 42.7053 462.501 46.5C470.501 52.5 463.138 72.5 448.002 77C428.143 82.9045 439 65 455 52.5C471 40 488 28.5 496.5 25" stroke="black" stroke-opacity="0.61" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
+  `
+}
